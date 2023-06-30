8948ff8daa84f0022da84df7a99424af89bbca03
Infinitay <Infinitay@users.noreply.github.com>
2023-06-30 18:10:14 -0400

    Naming and metadata refactor

--- src/test/java/treecount/TreeCountPluginTest.java
+++ src/test/java/treecount/TreeCountPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package treecount;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class TreeCountPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(TreeCountPlugin.class);
 		RuneLite.main(args);
 	}
 }
